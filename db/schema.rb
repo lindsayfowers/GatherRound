ed1ff5f70735e98cee665c0d45c0b98529a7ae71
lindsayfowers <lfowers@gmail.com>
2017-03-20 09:25:23 -0600

    Updating status for enum on Posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316222733) do
+ActiveRecord::Schema.define(version: 20170320150718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,9 +30,10 @@ ActiveRecord::Schema.define(version: 20170316222733) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "slug"
+    t.integer  "status",     default: 0
     t.index ["slug"], name: "index_posts_on_slug", unique: true, using: :btree
   end
 
